7dc8fd6dec9a216095007113071da482f354c871
Samujjwal Kumar <kumarsamujjwal@gmail.com>
2025-10-02 19:32:46 -0400

    Added config for pulling API endpoint, updated angular.json to include the config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 
 import { routes } from './app.routes';
 import { API_BASE_URL } from './api-tokens';
 import { accessCodeInterceptor } from './interceptors/access-code.interceptor';
+import { firstValueFrom } from 'rxjs';
+
+function initializeRuntimeConfig() {
+  return provideAppInitializer(async () => {
+    try {
+      // you can use DI here; initializers run in an injection context
+      const http = inject(HttpClient);
+      const cfg = await firstValueFrom(http.get<{ apiBaseUrl?: string }>('/assets/config.json'));
+      (window as any).__API_BASE_URL__ = cfg.apiBaseUrl ?? '';
+    } catch {
+      (window as any).__API_BASE_URL__ = '';
+    }
+  });
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-     provideHttpClient(withInterceptors([accessCodeInterceptor])),
-    { provide: API_BASE_URL, useValue: 'http://localhost:8000' }
+    provideHttpClient(withInterceptors([accessCodeInterceptor])),
+
+    initializeRuntimeConfig(),
+
+    { provide: API_BASE_URL, useFactory: () => (window as any).__API_BASE_URL__ ?? '' }
   ]
 };
